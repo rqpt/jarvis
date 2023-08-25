7ddd3b770bb20b5b732147e835516ceaa212ae23
Ernst <ernst@cubezoo.joburg>
2023-08-26 01:29:50 +0200

    feat: Add audio recording and transcription functionality

--- voice-assistant.sh
+++ voice-assistant.sh
@@ -34,17 +34,14 @@ session_arg=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)
 
 while true; do
 
-  # Start audio recording and save as tmp.mp3
   arecord -f cd -t wav -r 44100 | lame -r - tmp.mp3
 
-  # Send audio file for transcription and extract text using jq
   api_response=$(curl -s https://api.openai.com/v1/audio/transcriptions \
     -H "Authorization: Bearer $OPENAI_API_KEY" \
     -H "Content-Type: multipart/form-data" \
     -F "file=@tmp.mp3" \
     -F "model=whisper-1")
 
-  # Remove tmp.mp3 file
   rm tmp.mp3
 
   if [ "$response" = "1" ]; then
