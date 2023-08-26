5dfc26b0d270972e43a5cf19cda22c1f4b023716
Ernst <ernst@cubezoo.joburg>
2023-08-26 10:51:55 +0200

    refactor: Rename voice-assistant.sh to jarvis.sh and update file paths in script

--- jarvis.sh
+++ jarvis.sh
@@ -33,27 +33,30 @@ session_arg=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)
 while true; do
   echo "Say something ;)"
   echo "Press (ctrl + c) when you're done."
-  echo ""
 
-  arecord -f cd -t wav -r 44100 | lame -r - $HOME/desktop/tmp.mp3
+  ffplay -nodisp -hide_banner -autoexit $HOME/projects/jarvis/sound/human-prompt.mp3 2> /dev/null & arecord -q -f cd -t wav -r 44100 | lame -r - $HOME/projects/jarvis/sound/tmp.mp3
   echo ""
 
   api_response=$(curl -s https://api.openai.com/v1/audio/transcriptions \
     -H "Authorization: Bearer $OPENAI_API_KEY" \
     -H "Content-Type: multipart/form-data" \
-    -F "file=@$HOME/desktop/tmp.mp3" \
+    -F "file=@$HOME/projects/jarvis/sound/tmp.mp3" \
     -F "model=whisper-1")
 
-  rm $HOME/desktop/tmp.mp3
+  rm $HOME/projects/jarvis/sound/tmp.mp3
 
   if [ "$response" = "1" ]; then
+    ffplay -nodisp -hide_banner -autoexit $HOME/projects/jarvis/sound/assistant-prompt.mp3 2> /dev/null
     echo $api_response | jq -r '.text' | sgpt --chat $session_arg
     echo ""
   else
     ai_text_response=$(echo $api_response | jq -r '.text' | sgpt --chat $session_arg)
-    gtts-cli "$ai_text_response" -o $HOME/desktop/output.mp3
-    ffplay -nodisp -hide_banner -autoexit $HOME/desktop/output.mp3
-    rm $HOME/desktop/output.mp3
+    gtts-cli "$ai_text_response" -o $HOME/projects/jarvis/sound/output.mp3
+    ffplay -nodisp -hide_banner -autoexit $HOME/projects/jarvis/sound/assistant-prompt.mp3 2> /dev/null
+    ffplay -nodisp -hide_banner -autoexit $HOME/projects/jarvis/sound/output.mp3 2> /dev/null
+    rm $HOME/projects/jarvis/sound/output.mp3
   fi
+
+  sleep 2s
 
 done
