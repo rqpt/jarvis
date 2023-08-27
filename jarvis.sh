c63a2b599012466678256c82f14b90d2d09eee83
Ernst Vermeulen <1ernstvermeulen1@gmail.com>
2023-08-27 11:56:06 +0200

    refactor: Update color variables in jarvis.sh

--- jarvis.sh
+++ jarvis.sh
@@ -11,9 +11,9 @@ echo "|                                                           |"
 echo "|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|"
 echo ""
 
-BLUE='\033[1;34m'
-GREEN="\e[38;2;137;207;153m"
-RESET='\033[0m'
+PROMPT_COLOUR='\033[1;34m'
+RESPONSE_COLOUR="\e[1;35m"
+RESET_COLOUR='\033[0m'
 
 ROOT_DIR=$HOME/projects/jarvis
 SOUND_DIR=$ROOT_DIR/sound
@@ -21,16 +21,16 @@ SOUND_DIR=$ROOT_DIR/sound
 session_arg=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)
 
 while true; do
-  echo -e -n "${BLUE}"
+  echo -e -n "${PROMPT_COLOUR}"
   echo -e "\e[4mYou\e[0m"
-  echo -e "${RESET}"
+  echo -e "${RESET_COLOUR}"
 
-  echo -e -n "${BLUE}"
+  echo -e -n "${PROMPT_COLOUR}"
   arecord -d 600 -q -f cd -t wav -r 44100 > $SOUND_DIR/tmp.wav &
   read input
 
   kill $!
-  echo -e -n "${RESET}"
+  echo -e -n "${RESET_COLOUR}"
 
   if [[ -z $input ]]; then
     echo "🎤"
@@ -46,23 +46,22 @@ while true; do
     rm $SOUND_DIR/tmp.mp3
   fi
 
-  echo -e "${GREEN}"
+  echo -e "${RESPONSE_COLOUR}"
   echo -e "\e[4mJarvis\e[0m"
-  echo -e "${RESET}"
+  echo -e "${RESET_COLOUR}"
 
-  echo -e -n "${GREEN}"
-  if [[ -z $input ]]; then
-    echo $api_response | jq -r '.text' | sgpt --chat $session_arg | tee $HOME/projects/jarvis/ai-text-response
-  else
-    echo $input | sgpt --chat $session_arg | tee $HOME/projects/jarvis/ai-text-response
-  fi
-  echo -e "${RESET}"
+  echo -e -n "${RESPONSE_COLOUR}"
 
   if [[ -z $input ]]; then
+    echo $api_response | jq -r '.text' | sgpt --chat $session_arg | tee $ROOT_DIR/ai-text-response
     cat $HOME/projects/jarvis/ai-text-response | festival --tts
+    rm $ROOT_DIR/ai-text-response
+  else
+    echo $input | sgpt --chat $session_arg
   fi
 
-  rm $ROOT_DIR/ai-text-response
+  echo -e "${RESET_COLOUR}"
+
   rm $SOUND_DIR/tmp.wav
 
   sleep 3s
